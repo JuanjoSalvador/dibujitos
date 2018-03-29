1b954c0c82afe8783457b6f31f7d71a992457189
Juan D. Jara <juanorigami@gmail.com>
2018-03-29 21:24:03 +0200

    add full calendar from new api

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import moment from 'moment';
 
-const endpoint = "https://hs.fuken.xyz";
+const endpoint = "https://nyapi.fuken.xyz";
 const List = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
+  h3 {
+    margin-left: 20px;
+    margin-bottom: 0;
+    margin-top: 30px;
+    font-weight: normal;
+    font-size: 20px;
+  }
   li {
     padding: 0 20px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin: 15px 0;
     a {
       flex: 1;
       margin-right: 10px;
+    }
+    p {
+      margin: 0;
     }
   }
 `;
@@ -31,26 +42,53 @@ const Title = styled.header`
 `;
 const Container = styled.div`
   min-width: 320px;
+  overflow-y: auto;
+  max-height: calc(100vh - 60px);
   @media(max-width: 1090px) {
     display: none;
   }
 `;
+
+const dayMap = {
+  'Monday': 'Lunes',
+  'Tuesday': 'Martes',
+  'Wednesday': 'Miércoles',
+  'Thursday': 'Jueves',
+  'Friday': 'Viernes',
+  'Saturday': 'Sábado',
+  'Sunday': 'Domingo',
+  'Today': 'Hoy'
+}
 
 class Calendar extends Component {
   state = {
     calendar: []
   }
   componentDidMount() {
-    const url = `${endpoint}/calendar_today`;
+    const url = `${endpoint}/calendar`;
     window.fetch(url).then(res => res.json())
     .then(json => {
-      this.setState({calendar: json});
+      this.setState({
+        calendar: this.filterCalendar(json)
+      });
     });
   }
   formatTime(timeStr) {
-    const time = moment.utc(timeStr, 'HH:mm');
-    time.add(9, 'hours');
-    return time.format('HH:mm');
+    return moment(timeStr).format('HH:mm');
+  }
+  filterCalendar(calendar) {
+    return calendar.filter(group => {
+      const startOfDay = moment(group.day, 'dddd');
+      if(group.day === 'Sunday') {
+        startOfDay.add(1, 'week')
+      }
+      return startOfDay.isSameOrAfter(moment(), 'day')
+    }).map(group => {
+      if (moment(group.day, 'dddd').isSame(moment(), 'day')) {
+        group.day = 'Today';
+      }
+      return group;
+    })
   }
 
   render() { 
@@ -58,16 +96,19 @@ class Calendar extends Component {
       <Container>
         <Title>
           <h2>Calendario</h2>
-          <p>Horario de emision de capitulos de hoy</p>
+          <p>Horario de emision de capitulos de HorribleSubs</p>
         </Title>
-        <List>
-          {this.state.calendar.map(item => (
-            <li key={item.link}>
-              <a href={item.link}>{item.title}</a>
-              <p>{this.formatTime(item.time)}</p>
-            </li>
-          ))}
-        </List>
+        {this.state.calendar.map(group => (
+          <List key={group.day}>
+            <h3>{dayMap[group.day]}</h3>
+            {group.animes.map(item => (
+              <li key={item.link}>
+                <a href={item.link}>{item.title}</a>
+                <p>{this.formatTime(item.time)}</p>
+              </li>
+            ))}
+          </List>
+        ))}
       </Container>
     );
   }
